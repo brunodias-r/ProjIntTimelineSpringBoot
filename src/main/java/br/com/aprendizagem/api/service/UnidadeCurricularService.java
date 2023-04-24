e52e519ddde5e493e037ed6e6ff1e6627929b9b0
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-04-24 19:28:42 -0300

    Refactor: Alterações no service de unidade curricular.

--- src/main/java/br/com/aprendizagem/api/service/UnidadeCurricularService.java
+++ src/main/java/br/com/aprendizagem/api/service/UnidadeCurricularService.java
@@ -1,50 +1,46 @@
 package br.com.aprendizagem.api.service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.transaction.Transactional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.aprendizagem.api.entity.Modulo;
 import br.com.aprendizagem.api.entity.UnidadeCurricular;
-import br.com.aprendizagem.api.repository.ModuloRepository;
 import br.com.aprendizagem.api.repository.UnidadeCurricularRepository;
+import br.com.aprendizagem.api.response.UnidadeCurricularResponse;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class UnidadeCurricularService {
 
-    @Autowired
-    private ModuloRepository moduloRepository;
-
-    @Autowired
     private UnidadeCurricularRepository unidadeCurricularRepository;
 
     private static Map<Long, UnidadeCurricular> listaUnidadesCurriculares = new HashMap<>();
 
-    public ResponseEntity<UnidadeCurricular> listarUnidadesCurricularesPorModulo(Modulo modulo) {
+    @Transactional
+    public ResponseEntity<UnidadeCurricular> listarUnidadeCurricularPorModulo(Modulo modulo) {
         UnidadeCurricular unidadeCurricular = listaUnidadesCurriculares.get(modulo);
         if (unidadeCurricular == null) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(unidadeCurricular);
     }
-
     
-
-    // @Transactional
-    // public ResponseEntity<EstudanteResponse> buscarEstudadePorId(Long id) {
-    // Optional<Estudante> estudanteOpt = estudanteRepository.findById(id);
-    // if (!estudanteOpt.isPresent()) {
-    // return ResponseEntity.notFound().build();
-    // }
-    //
-    // Hibernate.initialize(estudanteOpt.get().getLivros());
-    // return ResponseEntity.ok(EstudanteResponse.of(estudanteOpt.get()));
-    // }
+    @Transactional
+	public ResponseEntity<List<UnidadeCurricularResponse>> listarUnidadesCurriculares() {
+		List<UnidadeCurricular> unidadesCurriculares = unidadeCurricularRepository.findAll();
+		if (unidadesCurriculares.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} 
+		return ResponseEntity.ok(UnidadeCurricularResponse.of(unidadesCurriculares));
+		//return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+    
 }

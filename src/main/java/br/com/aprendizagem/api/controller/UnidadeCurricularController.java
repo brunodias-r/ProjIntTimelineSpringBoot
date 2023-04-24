5b4c598f706d25805798158ede9e52f0e0f2398d
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-04-24 19:27:47 -0300

    Refactor: Alterações no controller de unidade curricular.

--- src/main/java/br/com/aprendizagem/api/controller/UnidadeCurricularController.java
+++ src/main/java/br/com/aprendizagem/api/controller/UnidadeCurricularController.java
@@ -1,10 +1,18 @@
 package br.com.aprendizagem.api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import br.com.aprendizagem.api.entity.UnidadeCurricular;
+import br.com.aprendizagem.api.response.UnidadeCurricularResponse;
+import br.com.aprendizagem.api.entity.Modulo;
 import br.com.aprendizagem.api.service.UnidadeCurricularService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -12,7 +20,30 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/unidadeCurricular")
 public class UnidadeCurricularController {
     
-    @Autowired
-    private UnidadeCurricularService unidadeCurricularService;
+    UnidadeCurricularService unidadeCurricularService;
+    
+    @GetMapping("buscar/{modulo}")
+	@ApiOperation(value = "Buscar unidade curricular por módulo")
+	@ApiResponses(
+			{
+				@ApiResponse(code = 401, message = "Acesso não autorizado."),
+				@ApiResponse(code = 403, message = "Proibido."),
+				@ApiResponse(code = 404, message = "Não encontrado."),
+			})
+    public ResponseEntity<UnidadeCurricular> listarUnidadeCurricularPorModulo(Modulo modulo){
+    	return unidadeCurricularService.listarUnidadeCurricularPorModulo(modulo);
+    }
+    
+    @GetMapping("/listar")
+	@ApiOperation(value = "Listar todas as unidades curriculares")
+	@ApiResponses(
+			{
+				@ApiResponse(code = 401, message = "Acesso não autorizado."),
+				@ApiResponse(code = 403, message = "Proibido."),
+				@ApiResponse(code = 404, message = "Não encontrado."),
+			})
+    public ResponseEntity<List<UnidadeCurricularResponse>> listarUnidadesCurriculares(){
+    	return unidadeCurricularService.listarUnidadesCurriculares();
+    }
 
 }
